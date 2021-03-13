0408f13297772951a48b72849ea011ef312e598b
robotchaox <robotchaox@qq.com>
2021-03-14 01:08:14 +0800

    format indent

--- Chapter7/02_canny_edge_detection.cpp
+++ Chapter7/02_canny_edge_detection.cpp
@@ -6,24 +6,22 @@ using namespace std;
 using namespace cv;
 using namespace cv::cuda;
 
-
 int main()
 {
-    Mat h_image = imread("images/drawing.JPG",0);
-    if (h_image.empty())
-    {
-        cout << "can not open image"<< endl;
-        return -1;
-    }
+	Mat h_image = imread("images/drawing.JPG",0);
+	if (h_image.empty())
+	{
+		cout << "can not open image" << endl;
+		return -1;
+	}
 	GpuMat d_edge,d_image;
-    Mat h_edge;
+	Mat h_edge;
 	d_image.upload(h_image);
 	cv::Ptr<cv::cuda::CannyEdgeDetector> canny_edge = cv::cuda::createCannyEdgeDetector(2.0, 100.0, 3, false);
 	canny_edge->detect(d_image, d_edge);
-    d_edge.download(h_edge);
-imshow("source", h_image);
-    imshow("detected edges", h_edge);
-	 waitKey(0);
-
-    return 0;
+	d_edge.download(h_edge);
+	imshow("source", h_image);
+	imshow("detected edges", h_edge);
+	waitKey(0);
+	return 0;
 }
